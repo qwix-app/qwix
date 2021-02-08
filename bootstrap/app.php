a967274e46c18dc67329c853668211252f003559
Dan <dummy4dani@gmail.com>
2021-02-07 22:30:40 -0300

    Register flipbox/lumen-generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,6 +95,8 @@ $app->configure('app');
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
