bfaf57c5296ebc6c83db4002a4e025401c2cdace
Dan <dummy4dani@gmail.com>
2021-02-08 23:16:05 -0300

    Implement User CRUD operations

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use ErrorException;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -32,40 +35,78 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, User::getRules());
+
+        $request->merge([
+            'password' =>
+                Hash::make($request->get('password'))
+        ]);
+
+        $user = User::create($request->all());
+
+        return new UserResource($user);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\User  $user
+     * @param  int  $userId
      * @return \Illuminate\Http\Response
      */
-    public function show(User $user)
+    public function show(int $userId)
     {
-        //
+        try {
+            $user = User::findOrFail($userId);
+    
+            return new UserResource($user);
+        } catch (ModelNotFoundException $e) {
+            abort(404, "A user with id #$userId could not be found.");
+        }
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
+     * @param  int  $userId
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, int $userId)
     {
-        //
+        $this->validate($request, User::getRules());
+
+        try {
+            $user = User::findOrFail($userId);
+
+            $user->fill($request->all());
+
+            if ($user->isClean())
+                abort(422, "No changes made to user type #$userId");
+
+            $user->save();
+
+            return new UserResource($user);
+        } catch (ModelNotFoundException $e) {
+            abort(404, "User with id #$userId could not be found.");
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\User  $user
+     * @param  int  $userId
      * @return \Illuminate\Http\Response
      */
-    public function destroy(User $user)
+    public function destroy(int $userId)
     {
-        //
+        try {
+            $user = User::findOrFail($userId);
+
+            $user->delete();
+
+            return new UserResource($user);
+        } catch (ModelNotFoundException $e) {
+            abort(404, "User #$userId was not found.");
+        }
     }
 }
